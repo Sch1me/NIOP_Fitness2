9c2913d83594219b334771e32e5c226351694aba
SimeZorki <simebusiness7@gmail.com>
2023-11-08 18:28:14 +0100

    PodatkovniKontekst

---
+++ NIOP_Fitness/PodatkovniKontekst.cs
@@ -0,0 +1,17 @@
+﻿using NIOP_Fitness.Objektni_Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NIOP_Fitness
+{
+    internal class PodatkovniKontekst
+    {
+        public List<Osoba> osobe;
+        public List<Vjezbe> vjezbe;
+        public List<Trening> treninzi;
+
+    }
+}
